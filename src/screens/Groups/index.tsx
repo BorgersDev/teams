efbb0f100709db12cb365cace904a6b813a089a2
BorgesDev <borgesarthur.dev@gmail.com>
2024-06-03 18:12:07 -0300

    Header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container } from './styles'
+import { Header } from '@components/Header'
 
 export const Groups = () => {
   return (
     <Container > 
-     
+      <Header showBackButton />
     </Container>
   )
 }
