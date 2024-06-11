e682710b6ba4ba0752664c85412f7378d7b10668
BorgesDev <borgesarthur.dev@gmail.com>
2024-06-11 13:35:45 -0300

    Other components and Players screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'react-native';
-import Groups from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import theme from '@theme/index';
+import {Players} from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor='transparent' 
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <Players /> : <Loading /> }
     </ThemeProvider>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,6 +5,7 @@ import { CaretLeft } from 'phosphor-react-native'
 export const Container = styled.View`
   width: 100%;
 
+  margin-top: 24px;
   flex-direction: row;
   align-items: center;
   justify-content: center
