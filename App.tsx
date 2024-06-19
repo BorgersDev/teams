0e8c1f4bf1245bb7aed3128b909eb5e5c647b666
BorgesDev <borgesarthur.dev@gmail.com>
2024-06-19 08:22:33 -0300

    implementing navigation using Stack Navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import theme from '@theme/index';
-import {Players} from '@screens/Players';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor='transparent' 
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
